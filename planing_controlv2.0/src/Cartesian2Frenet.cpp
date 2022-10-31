6528c74d51ed24e39a27723b1aed3d5bbbf13735
Scottyang3 <82214262+Scottyang3@users.noreply.github.com>
2022-10-31 22:14:32 +0800

    Add files via upload

---
+++ planing_controlv2.0/src/Cartesian2Frenet.cpp
@@ -0,0 +1,176 @@
+#include "Cartesian2Frenet.h"
+
+bool Cartesian2Frenet::fcn_index2s(std::vector<xy_points_info> path, 
+                                   xy_points_info origin_path, 
+                                   int origin_match_point_index,
+                                   std::vector<double> *index2s){
+    // 该函数将输出index与s的转换关系，index2s表示当path_x,path_y的离散点的编号为i时，对应的弧长为index2s(i)
+    // 输入 path_x path_y 待转化的离散点的集合
+    //      origin_x,y frenet原点在世界坐标系下的坐标
+    //      origin_match_point_index 原点的匹配点的编号
+    // path点的个数
+    // 输出初始化
+    int n=180;
+    index2s->resize(n);
+    // 首先计算以path起点为坐标原点的index2s
+    for (size_t i = 1; i < n; i++)
+    {
+        index2s->at(i)=std::sqrt(std::pow((path[i].x-path[i-1].x),2)+std::pow((path[i].y-path[i-1].y),2)+index2s->at(i-1));
+    }
+    // 再计算以轨迹起点到frenet_path的坐标原点的弧长，记为s0，再用index2s - s0 就是最终的结果
+    // 计算s0
+    // s_temp frenet原点的匹配点的弧长
+    double s_temp;
+    s_temp=index2s->at(origin_match_point_index);
+    // 判断原点在匹配点的前面还是后面
+    // 两个向量match_point_to_origin 
+    //        match_point_to_match_point_next
+    std::vector<double>vector_match_2_origin,vector_match_2_match_next;
+    //origin_path投影点只有一个，所以只用[0]
+    vector_match_2_origin[0]=origin_path.x-path[origin_match_point_index].x;
+    vector_match_2_origin[1]=origin_path.y-path[origin_match_point_index].y;
+    vector_match_2_match_next[0]=path[origin_match_point_index+1].x-path[origin_match_point_index].x;
+    vector_match_2_match_next[1]=path[origin_match_point_index+1].y-path[origin_match_point_index].y;
+
+    double s0;
+    if (vector_match_2_origin[0]*vector_match_2_match_next[0]+vector_match_2_origin[1]*vector_match_2_match_next[1]>0)
+    {
+        //坐标原点在匹配点的前面
+        s0=s_temp + std::sqrt(vector_match_2_origin[0]*vector_match_2_origin[0]+vector_match_2_origin[1]*vector_match_2_origin[1]);
+    }
+    else{
+        s0=s_temp - std::sqrt(vector_match_2_origin[0]*vector_match_2_origin[0]+vector_match_2_origin[1]*vector_match_2_origin[1]);
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        index2s->at(i)=index2s->at(i)-s0;
+    }
+}
+
+
+
+double CalcSFromIndex2S(std::vector<double> index2s, std::vector<xy_points_info> frenet_path, double proj_x, double proj_y,int proj_match_point_index){
+    //该函数将计算当指定index2s的映射关系后，计算点proj_x,proj_y的弧长
+    std::vector<double> vector_1(2);
+    vector_1[0]=proj_x-frenet_path[proj_match_point_index].x;
+    vector_1[1]=proj_y-frenet_path[proj_match_point_index].y;
+    std::vector<double> vector_2(2);
+    if (proj_match_point_index < frenet_path.size())
+    {
+        vector_2[0]=frenet_path[proj_match_point_index+1].x-frenet_path[proj_match_point_index].x;
+        vector_2[1]=frenet_path[proj_match_point_index+1].y-frenet_path[proj_match_point_index].y;
+    }
+    else{
+        vector_2[0]=frenet_path[proj_match_point_index].x-frenet_path[proj_match_point_index].x;
+        vector_2[1]=frenet_path[proj_match_point_index].x-frenet_path[proj_match_point_index].x;
+    }
+    double s;
+    if (vector_1[0]*vector_1[0]+vector_1[1]*vector_1[1] > 0)
+    {
+        s = index2s[proj_match_point_index] + std::sqrt(vector_1[0]*vector_1[0]+vector_1[1]*vector_1[1]);
+    }
+    else{
+        s = index2s[proj_match_point_index] + std::sqrt(vector_1[0]*vector_1[0]+vector_1[1]*vector_1[1]);
+    }
+    return s;
+}
+
+bool Cartesian2Frenet::Calc_SL(double x_set, double y_set, 
+                               std::vector<xy_points_info> frenet_path, 
+                               std::vector<proj_points_info> proj_points, 
+                               std::vector<double> index2s,
+                               std::vector<double> *s_set,
+                               std::vector<double> *l_set){
+    // 该函数将计算世界坐标系下的x_set，y_set上的点在frenet_path下的坐标s l
+    // 输入 x_set,y_set 待坐标转换的点
+    //      frenet_path_x,frenet_path_y   frenet坐标轴
+    //      proj_x,y,heading,kappa,proj_match_point_index 待坐标转换的点的投影点的信息
+    //      index2s   frenet_path的index与s的转换表
+    // 由于不知道有多少个点需要做坐标转换，所以需要做缓冲
+    int n=128;//最多处理的128个点
+    // std::vector<double> s_set(n,Inf);
+    // std::vector<double> l_set(n,Inf);
+    s_set->resize(n,Inf);
+    l_set->resize(n,Inf);
+
+    for (size_t i = 0; i < s_set->size(); i++)
+    {
+        if (s_set->at(i)==Inf)
+        {
+            break;
+        }
+        //计算s
+        s_set->at(i) = CalcSFromIndex2S(index2s, frenet_path, proj_points[i].proj_point_x, proj_points[i].proj_point_y, proj_points[i].proj_match_index);
+        l_set->at(i) = -(x_set-proj_points[i].proj_point_x)*sin(proj_points[i].proj_heading)+(y_set-proj_points[i].proj_point_y)*cos(proj_points[i].proj_heading);
+    }    
+}
+
+bool Cartesian2Frenet::Calc_sl_dot(std::vector<double> l_set,
+                                   double plan_start_vx,
+                                   double plan_start_vy,
+                                   double proj_heading,
+                                   double proj_kappa,
+                                   std::vector<double> *s_dot_set,
+                                   std::vector<double> *l_dot_set,
+                                   std::vector<double> *dl_set){
+    //该函数将计算frenet坐标系下的s_dot, l_dot, dl/ds
+    int n=128;
+    //输出初始化
+    s_dot_set->resize(n,Inf);
+    l_dot_set->resize(n,Inf);
+    dl_set->resize(n,Inf);
+    for (size_t i = 0; i < l_set.size(); i++)
+    {
+        if (l_set[i]==Inf)
+        {
+            break;
+        }
+        l_dot_set->at(i)=-plan_start_vx*sin(proj_heading)+plan_start_vy*cos(proj_heading);
+        s_dot_set->at(i)=plan_start_vx*cos(proj_heading)+plan_start_vy*sin(proj_heading);
+        // 向量法做cartesian与frenet的转换要更简单，但是也有缺点，向量法必须依赖速度加速度
+        // l' = l_dot/s_dot 但是如果s_dot = 0 此方法就失效了
+        if (std::abs(s_dot_set->at(i))<1e-6)
+        {
+            dl_set->at(i)=0;
+        }
+        else{
+            dl_set->at(i)=l_dot_set->at(i)/s_dot_set->at(i);
+        }
+    }
+}
+
+bool Cartesian2Frenet::Calc_sl_dot2(double plan_start_ax,
+                                    double plan_start_ay,
+                                    double proj_heading,
+                                    double proj_kappa,
+                                    std::vector<double> l_set,
+                                    std::vector<double> s_dot_set,
+                                    std::vector<double> dl_set,
+                                    std::vector<double> *s_dot2_set,
+                                    std::vector<double> *l_dot2_set,
+                                    std::vector<double> *ddl_set){
+    // 由于不知道有多少个点需要做坐标转换，设一个最大值做缓冲
+    int n=128;
+    // 输出初始化
+    s_dot2_set->resize(n,Inf);
+    l_dot2_set->resize(n,Inf);
+    ddl_set->resize(n,Inf);
+    for (size_t i = 0; i < l_set.size(); i++)
+    {
+        if (l_set[i]==Inf)
+        {
+            break;
+        }
+        l_dot2_set->at(i)=-plan_start_ax*sin(proj_heading)+plan_start_ay*cos(proj_heading)-proj_kappa*(1-proj_kappa*l_set[i]*s_dot_set[i]*s_dot_set[i]);
+        s_dot2_set->at(i)=(1/(1-proj_kappa*l_set[i]))*(plan_start_ax*cos(proj_heading)+plan_start_ay*sin(proj_heading)+2*proj_kappa*dl_set[i]*s_dot_set[i]*s_dot_set[i]);
+        // 向量法做cartesian与frenet的转换要更简单，但是也有缺点，向量法必须依赖速度加速度
+        // l' = l_dot/s_dot 但是如果s_dot = 0 此方法就失效了
+        if (std::abs(s_dot_set[i])<1e-6)
+        {
+            ddl_set->at(i)=0;
+        }
+        else{
+            ddl_set->at(i)=(l_dot2_set->at(i)-dl_set[i]*s_dot2_set->at(i))/std::pow(s_dot_set[i],2);
+        }
+    }
+}
---
+++ planing_controlv2.0/src/main.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include "struct_common.h"
+#include "Referenceline.h"
+#include "Load_Roadmap.h"
+#include "calc_plan_stitch_trajectory.h"
+#include "Cartesian2Frenet.h"
+#include "LQR_control.h"
+#include "lqr_control.h"
+#include "Obstacle.h"
+#include "matplotlibcpp.h"
+
+
+namespace plt = matplotlibcpp;
+
+location_info update_state(double steer, location_info car) {
+    car.host_x += car.host_speed * cos(car.host_heading_xy)* 0.01;
+    car.host_y += car.host_speed * sin(car.host_heading_xy)* 0.01;
+    car.host_heading_xy=car.host_heading_xy + car.host_speed/2.947*tan(steer)*0.01;
+    car.host_speed=30;
+    car.host_vx=0;
+    car.host_vy=0;
+    car.host_yawrate=0;
+    car.host_acceleration=0;
+    car.host_ax=0;
+    car.host_ay=0;
+	return car;
+}
+
+int main() {
+    plt::figure_size(1200, 800);
+    //------------------------------------------------------------//
+    //load roadmap
+    std::vector<xy_points_info> xy_points_;
+    xy_points_info xy_load;
+    PathPlanningNode loadpath;
+    std::string roadmap_path;
+    roadmap_path="/home/yj/le_materials/demo_test/data/path.txt";
+    loadpath.loadRoadmap(roadmap_path, &xy_points_);
+
+    //将点存如向量画图观察
+    std::vector<double> xx,yy;//画图全局路径
+    for (size_t i = 0; i < xy_points_.size(); i++)
+    {
+        xy_load.x=xy_points_[i].x;
+        xy_load.y=xy_points_[i].y;
+        xx.push_back(xy_points_[i].x);
+        yy.push_back(xy_points_[i].y);
+    }
+    //------------------------------------------------------------//
+
+    //------------------------------------------------------------//
+    //Get the start time
+    struct timeval time1,time2;
+    gettimeofday(&time1,NULL);
+    std::cout<<"s: "<<time1.tv_sec<<"  ms: "<<time1.tv_sec*1000+time1.tv_usec/1000<<std::endl;
+    
+    //------------------------------------------------------------//
+
+    //------------------------------------------------------------//
+    //Calculate heading accumulated_s kappa dkappas
+    ReferenceLine refline;//参考线
+    std::vector<double> headings, accumulated_s, kappas, dkappas;
+    refline.Clac_heading_kappa_info(xy_points_, &headings, &accumulated_s, &kappas, &dkappas);
+    std::vector<proj_points_info> proj_points;//声明一个类型为proj_points的变量来存投影点的数据
+    std::vector<xy_points_info> referenceline_xy_points;//声明一个类型为referenceline_xy_points的变量来存向前150米和向后30米的路径点
+    std::vector<xy_points_info> fem_smoothing_line;//用来存平滑过后的路径点
+    //画图数据记录
+
+    //------------------------------------------------------------//
+    // 初始本车信息
+    location_info ego_info;
+    ego_info.host_x= xy_points_[40].x;
+    ego_info.host_y= xy_points_[40].y;
+    ego_info.host_heading_xy=headings[40];
+    ego_info.host_speed=2;
+    ego_info.host_vx=0;
+    ego_info.host_vy=0;
+    ego_info.host_yawrate=0;
+    ego_info.host_acceleration=0;
+    ego_info.host_ax=0;
+    ego_info.host_ay=0;
+    //------------------------------------------------------------//
+    std::vector<double>ego_x,ego_y;
+    std::vector<double>fem_x,fem_y;
+
+    for (size_t i = 0; i < 800; i++){
+        //Get the current time
+        gettimeofday(&time2,NULL);
+        std::cout<<"s: "<<time2.tv_sec-time1.tv_sec<<"  ms: "<<time2.tv_sec*1000+time2.tv_usec/1000<<std::endl;
+        //------------------------------------------------------------//
+        //Calculate proj_match_index proj_point_x proj_point_y
+        refline.Clac_proj_points_info(ego_info.host_x,ego_info.host_y,xy_points_, &proj_points);
+        // std::cout<<"proj_match_index= "<<proj_points.proj_match_index<<"   proj_point_x= "<<proj_points.proj_point_x<<"   proj_point_y= "<<proj_points.proj_point_y<<std::endl;
+
+        //提取referenceline 取出向前150个点 和向后30个点 减少计算量
+        refline.intercept_referenceline(proj_points[0].proj_match_index,xy_points_,&referenceline_xy_points);
+
+        //输出查看参考线
+        std::vector<double> ref_x,ref_y;
+        for (size_t i = 0; i < referenceline_xy_points.size(); i++)
+        {
+            ref_x.push_back(referenceline_xy_points[i].x);
+            ref_y.push_back(referenceline_xy_points[i].y);
+        }
+        //------------------------------------------------------------//
+        //输出查看自车所在位置
+        ego_x.push_back(ego_info.host_x);
+        ego_y.push_back(ego_info.host_y);
+
+
+        //------------------------------------------------------------------------------//
+        // refline.Fem_smoothing(referenceline_xy_points, &fem_smoothing_line);
+        // for (size_t i = 0; i < fem_smoothing_line.size(); i++)
+        // {
+        //     fem_x.push_back(fem_smoothing_line[i].x);
+        //     fem_y.push_back(fem_smoothing_line[i].y);
+        // }
+        // std::vector<planning_trajectory> planning_path;
+        // planning_path.resize(fem_smoothing_line.size());
+        // ControlCmd cmd;
+        // std::vector<double> pheadings, paccumulated_s, pkappas, pdkappas;
+        // refline.Clac_heading_kappa_info(fem_smoothing_line, &pheadings, &paccumulated_s, &pkappas, &pdkappas);
+        // for (size_t i = 0; i < fem_smoothing_line.size(); i++)
+        // {
+        //     planning_path[i].trajectory_x = fem_smoothing_line[i].x;
+        //     planning_path[i].trajectory_y = fem_smoothing_line[i].y;
+        //     planning_path[i].trajectory_heading=pheadings[i];
+        //     planning_path[i].trajectory_kappa=pkappas[i];
+        //     planning_path[i].trajectory_accel=0.0;
+        //     planning_path[i].trajectory_speed=0.0;
+        //     planning_path[i].trajectory_time=1.0;
+        // }
+        //------------------------------------------------------------------------------//
+
+        //------------------------------------------------------------------------------//
+        std::vector<planning_trajectory> planning_path;
+        planning_path.resize(referenceline_xy_points.size());
+        ControlCmd cmd;
+        std::vector<double> pheadings, paccumulated_s, pkappas, pdkappas;
+        refline.Clac_heading_kappa_info(referenceline_xy_points, &pheadings, &paccumulated_s, &pkappas, &pdkappas);
+        for (size_t i = 0; i < referenceline_xy_points.size(); i++)
+        {
+            planning_path[i].trajectory_x = referenceline_xy_points[i].x;
+            planning_path[i].trajectory_y = referenceline_xy_points[i].y;
+            planning_path[i].trajectory_heading=pheadings[i];
+            planning_path[i].trajectory_kappa=pkappas[i];
+            planning_path[i].trajectory_accel=0.0;
+            planning_path[i].trajectory_speed=0.0;
+            planning_path[i].trajectory_time=1.0;
+        }
+        //------------------------------------------------------------------------------//
+        // Obstacle obs;
+
+        // lqr计算前轮控制转角
+        //深蓝
+        // LQR_control lqr_sl;
+        // double steer_angle_control = -lqr_sl.ComputeControlCommand(ego_info, planning_path, cmd);
+        // std::cout<<" sl--------steeer_angle=  "<<steer_angle_control << " rad \n \n"<<std::endl;
+        //老王
+        lqr_control lqr;
+        double steer_angle_control = lqr.ComputeControlCommand(ego_info, planning_path);
+        std::cout<<"---------steeer_angle=  "<<steer_angle_control << " rad"<<std::endl;
+        //------------------------------------------------------------//
+
+        // //轨迹拼接
+        // calc_plan_trajectory stitch_path;
+        // plan_start_info plan_start_points;
+        // std::vector<stitch_trajectory> stitch_points;
+        // std::vector<planning_trajectory> pre_planning_trajectory_info;
+        // stitch_path.calc_plan_stitchtrajectory(pre_planning_trajectory_info, ego_info, current_time, &stitch_points, &plan_start_points);
+        // // std::cout << "stitch_points大小= " << stitch_points.size() << std::endl;
+        // // for (size_t i = 0; i < stitch_points.size(); i++)
+        // // {
+        // //     std::cout<<"stitch_points[i].stitch_x= "<<stitch_points[i].stitch_x<<std::endl;
+        // // }
+
+        // //Cartesian2Frenet
+        // Cartesian2Frenet CarFte;
+        // //              std::vector<points_conf> frenet_path, 
+        // //              std::vector<proj_points> proj_points, 
+        // // std::vector<double> index2s;
+        // // std::vector<double> *s_set;
+        // // std::vector<double> *l_set;
+        // // CarFte.Calc_SL(ego_info.host_x,ego_info.host_y, referenceline_xy_points,proj_points,index2s,&s_set, &l_set);
+        
+        ego_info = update_state(steer_angle_control,ego_info);
+        // 画图部分
+        // plt::clf();
+        // plt::named_plot("map", xx, yy, "r:");   //全局地图
+        // plt::title("Road Map");
+        // plt::legend();
+        // plt::named_plot("referenceline",ref_x, ref_y, "b:");    //提取的参考线
+        // plt::named_plot("ego_xy",ego_x, ego_y, "r-*");  //自车所在位�?
+        // plt::legend();
+        // plt::named_plot("fem",fem_x, fem_y, "g:");  //平滑过后的曲�?
+        // plt::legend();
+        // plt::pause(0.01);
+
+        fem_smoothing_line.clear();
+        referenceline_xy_points.clear();
+        planning_path.clear();
+        fem_x.clear();
+        fem_y.clear();
+    }
+
+    // plt::figure_size(1200, 800);
+
+    plt::named_plot("map", xx, yy, "r:");   //全局地图
+    plt::title("Road Map");
+    plt::legend();
+    // plt::named_plot("referenceline",ref_x, ref_y, "b:");    //提取的参考线
+    plt::named_plot("ego_xy",ego_x, ego_y, "r-*");  //自车所在位置
+    plt::legend();
+    plt::named_plot("fem",fem_x, fem_y, "g:");  //平滑过后的曲线
+    plt::legend();
+    plt::show();
+}
